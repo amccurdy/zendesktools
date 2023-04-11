931f9d130b8c8292d4b3fca22f3f70dee7b18377
Adam McCurdy <amccurdy@zenoss.com>
2023-04-11 10:58:33 -0500

    updating to use python3

--- json_fetcher.py
+++ json_fetcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from getpass import getpass
 import requests
 import json
@@ -10,18 +10,18 @@ api_url = 'https://zenoss.zendesk.com/api/v2/users.json'
 with open('users.json', 'a') as file:
     initial_request = requests.get(api_url, auth=(username, password))
     initial_request_text = initial_request.text.encode('utf-8').strip()
-    file.write(initial_request_text+'\n')
-    initial_json = json.loads(str(initial_request_text))
+    file.write(initial_request_text.decode('utf-8')+'\n')
+    initial_json = json.loads(str(initial_request_text, 'utf-8'))
     done = 100
     total = initial_json['count']
     reqs = 1
 
 while done < total:
     reqs += 1
-    url = api_url + '?page=%s' % (int(reqs))
-    print 'Making request %s to %s' % (reqs, url)
+    url = api_url + '?page=' + str(reqs)
+    print('Making request %s to %s' % (reqs, url))
     next_request = requests.get(url, auth=(username, password))
     next_request_text = next_request.text.encode('utf-8').strip()
     with open('users.json','a') as file:
-        file.write(next_request_text+'\n')
+        file.write(next_request_text.decode('utf-8')+'\n')
     done += 100
